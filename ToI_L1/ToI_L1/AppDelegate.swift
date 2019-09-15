ebb9d445895d76e2281e503fe3e6820950616ebc
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-15 11:17:24 +0300

    +prj

---
+++ ToI_L1/ToI_L1/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ToI_L1
+//
+//  Created by Михаил Ковалевский on 12/09/2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+        print("-> Terminating app.")
+    }
+
+
+}
+
