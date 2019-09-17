6619de7757f80793a59836aa73029e88b80f9c5d
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-18 02:18:24 +0300

    + Railway encryption

--- ToI_L1/ToI_L1/ViewController.swift
+++ ToI_L1/ToI_L1/ViewController.swift
@@ -24,12 +24,147 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextFieldDelegate {
+    
+    private func checkRailwayKey(key: inout String) -> Bool{
+        var result: Bool = false
+        key = key.lowercased().components(separatedBy: digitsAlphabet.inverted).joined()
+        
+        if !key.isEmpty && Int(key) ?? 0 > 0 {
+            result = true
+        }
+        
+        return result
+    }
+    
+    private func normalizedEnString(str: String) -> String{
+        return str.lowercased().components(separatedBy: engAlphabet.inverted).joined()
+    }
+    
+    private func normalizedRuString(str: String) -> String{
+        return str.lowercased().components(separatedBy: ruAlphabet.inverted).joined()
+    }
+    
+    private func railwayEncryption(str: String, key: Int) -> String {
+        var newStr: String = ""
+        let matrixLength: Int = str.count
+        var mx: [[Character]] = Array(repeating: Array(repeating: " ", count: matrixLength), count: key)
+        
+        var iOffset: Int = 0
+        var di: Int = 1
+        for j in 0...matrixLength-1 {
+            mx[iOffset][j] = str[str.index(str.startIndex, offsetBy: j)]
+            if key>1 {
+                switch iOffset {
+                case 0:
+                    di = 1
+                case (key-1):
+                    di = -1
+                default:
+                    break
+                }
+                iOffset = iOffset + di
+            }
+        }
+        
+        mx.forEach { (charArray) in
+            print(charArray)
+        }
+        
+        mx.forEach { (charArray) in
+            for i in 0...charArray.count-1 {
+                if engAlphabet.contains(charArray[i].unicodeScalars.first!) {
+                    newStr.append(charArray[i])
+                }
+            }
+        }
+        
+        return newStr
+    }
+    
+    private func vigenereEncryption(str: String, key: String) -> String {
+        var newStr: String = str
+        
+        return newStr
+    }
+    
+    private func playfairEncryption(str: String, key: String) -> String {
+        var newStr: String = str
+        
+        return newStr
+    }
+    
+    private let digitsAlphabet: CharacterSet = CharacterSet.init(charactersIn: "0"..."9")
+    private let ruAlphabet: CharacterSet = CharacterSet.init(charactersIn: "а"..."я")
+    private let engAlphabet: CharacterSet = CharacterSet.init(charactersIn: "a"..."z")
+    private let encTypes: [String] = ["Railway(en)", "Vigenère(ru)", "Playfair(en)"]
+    
+    @IBOutlet weak var encTypesPopUpButton: NSPopUpButton!
+    @IBOutlet weak var saveToFileCheckBox: NSButton!
+    
+    @IBOutlet weak var fileLabel: NSTextField!
+    
+    @IBOutlet weak var inputTextField: NSTextField!
+    @IBOutlet weak var keyTextField: NSTextField!
+    @IBOutlet weak var resultTextField: NSTextField!
+    
+    @IBAction func encryptAction(_ sender: Any){
+        var str: String = inputTextField.stringValue
+        let encType: String = encTypesPopUpButton.title
+        var key: String = keyTextField.stringValue
+        print("Encrypting msg: \(str)")
+        print("Encryption type: \(encType)")
+        print("Key: \(key)")
+        
+        let result: String
+        switch encType {
+        case encTypes[0]:
+            if checkRailwayKey(key: &key){
+                keyTextField.stringValue = key
+                keyTextField.textColor = .green
+                str = normalizedEnString(str: str)
+                inputTextField.stringValue = str
+                if !str.isEmpty {
+                    result = railwayEncryption(str: str, key: Int(key)!)
+                } else {
+                    result = ""
+                }
+            } else {
+                keyTextField.textColor = .systemPink
+                result = ""
+            }
+        case encTypes[1]:
+            result = vigenereEncryption(str: str, key: key)
+        case encTypes[2]:
+            result = playfairEncryption(str: str, key: key)
+        default:
+            result = "> Invalid encryption type!"
+        }
+        
+        resultTextField.stringValue = result
+    }
+    
+    @IBAction func decryptAction(_ sender: Any){
+        resultTextField.stringValue = "Anime!"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        keyTextField.delegate = self
+        prepareUI()
+    }
+    
+    private func prepareUI(){
+        encTypesPopUpButton.removeAllItems()
+        encTypesPopUpButton.addItems(withTitles: encTypes)
+    }
+    
+    func controlTextDidChange(_ obj: Notification) {
+        let textField = obj.object as? NSTextField
+        if textField?.identifier == keyTextField.identifier {
+            keyTextField.textColor = .white
+        }
     }
 
     override var representedObject: Any? {
