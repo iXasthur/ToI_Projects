02596f5775db9fdd7333f7ac190b3e9f5716bb4d
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-11-07 16:18:30 +0300

    +L3 proj

---
+++ ToI_L3/ToI_L3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToI_L3
+//
+//  Created by Михаил Ковалевский on 07.11.2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToI_L3/ToI_L3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToI_L3
+//
+//  Created by Михаил Ковалевский on 07.11.2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
