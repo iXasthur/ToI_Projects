306967d352791938202025cbf7632da54f2849f4
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-12-08 18:08:00 +0300

    L4 prj

---
+++ ToI_L4/ToI_L4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToI_L4
+//
+//  Created by Михаил Ковалевский on 08.12.2019.
+//  Copyright © 2019 Михаил Ковалевский. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToI_L4/ToI_L4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToI_L4
+//
+//  Created by Михаил Ковалевский on 08.12.2019.
+//  Copyright © 2019 Михаил Ковалевский. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
